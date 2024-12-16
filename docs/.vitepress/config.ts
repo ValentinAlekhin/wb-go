935e8877f58b1980335ea28f88710e4c6a62ea13
valentin <alekhin.v99@gmail.com>
2024-12-16 19:58:12 +0300

    Правка сборки документации

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'vitepress'
 export default defineConfig({
     title: 'wb-go',
     lang: 'ru-RU',
+    base: '/wb-go',
     description: 'Библиотека для работы с устройствами Wirenboard.',
     themeConfig: {
         search: {
